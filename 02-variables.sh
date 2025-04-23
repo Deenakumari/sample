1608a4eee345e8565ee5c187c02215252235675e
Deenakumari <deenamadhumanchi@gmail.com>
2025-04-23 20:40:39 +0530

    second-script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Deena:: Hello latha, How are you?"
+echo "latha:: hi deena, i am good. what about you?"
+echo "Deena:: i am fine too. how is your work?"
+echo "latha:: not bad. i am thinking to upgrade to devops" 
