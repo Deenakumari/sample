42475a720d23db7fdf4853c9917b8fcf09a839de
Deenakumari <deenamadhumanchi@gmail.com>
2025-04-23 20:51:00 +0530

    third-script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=Deena #no space between = and value
+PERSON2=latha
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: hi $PERSON1, i am good. what about you?"
+echo "$PERSON1:: i am fine too. how is your work?"
+echo "$PERSON2:: not bad. i am thinking to upgrade to devops"
