820effa238fceefe64e474bca7e507588482007f
Deenakumari <deenamadhumanchi@gmail.com>
2025-04-28 11:53:22 +0530

    some-script

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
    # echo "exit status: $1"
@@ -22,10 +25,10 @@ else
     echo "you are the super user."
 fi        
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 VALIDATE $? "installing mysql"
 
-dnf install git -y
+dnf install git -y &>>LOGFILE
 VALIDATE $? "installing git"
 
 echo "is this script running?"
