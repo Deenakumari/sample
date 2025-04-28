25a03294054a85d3ca10e94e99b34064f1b4bd27
Deenakumari <deenamadhumanchi@gmail.com>
2025-04-28 16:34:36 +0530

    some-script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   # echo "exit status: $1"
+   # echo "what are you doing: $2"
+   if [ $1 -ne 0 ]
+   then 
+       echo "$2...failure"
+       exit 1
+    else 
+        echo "$2...sucess"
+    fi    
+}
+
 if [ $USERID -ne 0 ]
 then 
     echo "please run this script as root user"
@@ -13,4 +29,11 @@ fi
 for i in $@
 do 
   echo "package to install: $i"
+  dnf list installed $i &>>$LOGFILE
+  if [ $? -eq 0]
+  then
+      echo "$i already installed...skipping"
+   else
+       echo "$i not installed.. need to install"   
+   fi
 done  
